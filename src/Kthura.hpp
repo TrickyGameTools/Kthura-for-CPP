33b3f995bd982dae32f9a1a10683778ba81cf5ff
Jeroen Broks <facebook@tbbs.nl>
2020-08-20 15:01:01 +0200

    Create Kthura.hpp

---
+++ src/Kthura.hpp
@@ -0,0 +1,7 @@
+#include "Kthura_Core.hpp"
+#include "Kthura_Draw.hpp"
+
+// You can choose to uncomment this if you wish to go for SDL
+// #include "Kthura_SDL_Driver.hpp"
+
+// This file is just for quick including... Nothing more.
