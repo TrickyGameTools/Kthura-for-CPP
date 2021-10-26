13c3720d0fb835380de616f41a0a4a5b6c43ecb6
Jeroen Broks <facebook@tbbs.nl>
2021-10-26 05:51:37 +0200

    Used Texture scanning

--- src/Kthura_Core.hpp
+++ src/Kthura_Core.hpp
@@ -1,7 +1,7 @@
 // Lic:
 // src/Kthura_Core.hpp
 // Kthura - Core (header)
-// version: 21.03.18
+// version: 21.10.11
 // Copyright (C) 2020, 2021 Jeroen P. Broks
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
